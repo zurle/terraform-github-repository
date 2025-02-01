6ec50c746dcbcfc801492787e08651f033cbc9a6
Santiago Zurletti <santiago.zur@gmail.com>
2025-02-01 14:06:43 -0300

    Fix module

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "repo_id" {
   description = "GitHub ID for the repository"
-  value       = github_repository.main.repo_id
+  value       = github_repository.repo.repo_id
 }
 
 output "repo_node_id" {
   description = "GraphQL global node id for use with v4 API"
-  value       = github_repository.main.node_id
+  value       = github_repository.repo.node_id
 }
 
 output "repo_name" {
   description = "The name of the repository."
-  value       = github_repository.main.name
+  value       = github_repository.repo.name
 }
 
